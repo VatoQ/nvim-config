606adfc91a9eac1b2e6c93643dbe76f3c1a7aef5
Jonathan Enns <jonathanenns97@web.de>
2025-07-30 18:01:41 +0200

    update colorscheme

--- lua/custom/plugins/tokyonight.lua
+++ lua/custom/plugins/tokyonight.lua
@@ -16,6 +16,16 @@ return { -- You can easily change to a different colorscheme.
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
+    -- vim.cmd.colorscheme 'tokyonight-night'
+    -- require('catppuccin').load()
+    --
+    for k in pairs(package.loaded) do
+      if k:match '.*onedark.*' then
+        package.loaded[k] = nil
+      end
+    end
+
+    require('onedark').setup()
+    require('onedark').colorscheme()
   end,
 }
