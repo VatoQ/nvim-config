948b29cb5ed9006d7e0f80512f10de396910baab
Jonathan Enns <jonathanenns97@web.de>
2025-08-01 00:55:39 +0200

    update colorscheme

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -3,4 +3,44 @@ return {
   lazy = true,
   name = 'catppuccin',
   priority = 1000,
+
+  config = function()
+    -- ---@diagnostic disable-next-line: missing-fields
+    --require('catppuccin').setup {
+    --  styles = {
+    --    comments = { italic = true },
+    --  },
+    --  -- custom_highlights = function(c)
+    --  --   return {
+    --  --     ['@lsp.typemod.function.builtin'] = { fg = C.peach },
+    --  --   }
+    --  -- end,
+    --}
+    require('catppuccin').setup {
+      flavour = 'macchiato',
+      auto_integrations = true,
+
+      background = {
+        light = 'latte',
+        dark = 'macchiato',
+      },
+
+      transparent_background = false,
+
+      show_end_of_buffer = true,
+
+      styles = {
+        comments = { 'bold' },
+      },
+      integrations = {
+        snacks = {
+          enabled = true,
+          picker_style = 'classic',
+        },
+        which_key = true,
+      },
+    }
+
+    require('catppuccin').load()
+  end,
 }
