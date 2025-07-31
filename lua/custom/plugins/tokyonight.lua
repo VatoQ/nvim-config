d1fb5e2674e32701b9623d53ac1d2a9da1bf21b2
Jonathan Enns <jonathanenns97@web.de>
2025-08-01 00:56:54 +0200

    update colotscheme

--- lua/custom/plugins/tokyonight.lua
+++ lua/custom/plugins/tokyonight.lua
@@ -5,29 +5,17 @@ return { -- You can easily change to a different colorscheme.
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   'folke/tokyonight.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
+
+  opts = {},
+
   config = function()
-    ---@diagnostic disable-next-line: missing-fields
     require('tokyonight').setup {
-      styles = {
-        comments = { italic = true },
-      },
+      style = 'moon',
+      terminal_colors = true,
+
+      sidebars = { 'qf', 'vista_kind', 'terminal', 'packer' },
     }
 
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    -- vim.cmd.colorscheme 'tokyonight-night'
-    -- require('catppuccin').load()
-    --
-
-    -- for k in pairs(package.loaded) do
-    --   if k:match '.*onedark.*' then
-    --     package.loaded[k] = nil
-    --   end
-    -- end
-
-    -- require('onedark').setup()
-    -- require('onedark').colorscheme()
-    require('tokyonight').load { style = 'night' }
+    require('tokyonight').load()
   end,
 }
