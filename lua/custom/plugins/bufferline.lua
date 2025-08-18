11202186d53907b89b7be28a5d6a2e4766846830
Jonathan Enns <jonathanenns97@web.de>
2025-08-18 19:43:46 +0200

    bufferline styling

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -10,21 +10,28 @@ return {
       options = {
         hover = {
           enabled = true,
-          delay = 150,
+          delay = 100,
           reveal = { 'close' },
         },
-        indicator = {
-          icon = '',
-          style = 'icon',
-        },
-        seperator_style = 'thick',
+        --indicator = {
+        --  icon = '',
+        --  style = 'none',
+        --},
+        buffer_close_icon = '󰅙',
+        close_icon = '󰅙 ',
+        -- seperator_style = { 'thick', 'slope' },
+        separator_style = 'slant',
+        -- enforce_regular_tabs = true,
         always_show_bufferline = true,
         tab_size = 20,
         diagnostics = 'nvim_lsp',
+        -- diagnostics_update_in_insert = true,
         offsets = {
           text_align = 'left',
           seperator = true,
         },
+
+        color_icons = true,
       },
     }
   end,
